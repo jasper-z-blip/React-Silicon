2f330ffda19542259f6da038510e87e46490edfe
Jasper <jaspersjohlen@live.se>
2024-11-08 11:11:47 +0100

    lägg till så det går att öppna frågan och den hamnar under

--- react/src/components/FAQItem.jsx
+++ react/src/components/FAQItem.jsx
@@ -1,19 +1,24 @@
-// FAQItem.js
 import React from 'react';
 
 const FAQItem = ({ question, answer, isOpen, onToggle }) => (
   <div className={`faq-item ${isOpen ? 'active' : ''}`}>
-    <h3>{question}</h3>
-    <a href="#" className="more" onClick={onToggle}>
-      <span className="btn-circle">
-        <i
-          className={`fa-solid fa-chevron-${isOpen ? 'up' : 'down'}`}
-        ></i>
-      </span>
-    </a>
-    {isOpen && <p>{answer}</p>}
+    <div className="question-row">
+      <h3>{question}</h3>
+      <a
+        href="#"
+        className="more"
+        onClick={(e) => {
+          e.preventDefault();
+          onToggle();
+        }}
+      >
+        <span className="btn-circle">
+          <i className={`fa-solid fa-chevron-${isOpen ? 'up' : 'down'}`}></i>
+        </span>
+      </a>
+    </div>
+    {isOpen && <p className="answer-text">{answer}</p>}
   </div>
 );
 
 export default FAQItem;
-
